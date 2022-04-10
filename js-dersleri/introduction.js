592df445d72dc4676a815cd1e4a424da4cb90db4
Cemalettin Altıntaş <cemalettinaltintas@gmail.com>
2022-04-10 17:04:01 +0300

    10 nisan ders sonu

---
+++ js-dersleri/introduction.js
@@ -0,0 +1,1 @@
+console.log('form introduction.js');
---
+++ js-dersleri/variables.js
@@ -0,0 +1,22 @@
+let firstName='Cemalettin'
+let lastName
+let isMarried=true
+//let age=null
+
+console.log(typeof firstName);
+console.log(typeof lastName);
+console.log(typeof isMarried);
+console.log(typeof age);
+
+// let adi='Cemalettin'
+// let soyadi='Altıntaş'
+// let medeniHal=true
+// let yas=38
+// let country='Türkiye'
+let adi='Oğuz',soyadi='Altıntaş',medeniHal=true,yas=38,country='Türkiye'
+console.log(adi,soyadi,medeniHal,yas,country);
+
+let myAge=35
+let yourAge=40
+console.log('I am '+myAge+' years old.');
+console.log('You are '+yourAge+' years old.');
